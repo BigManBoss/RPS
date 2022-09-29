9978686facb757ca3662741963e11367e037ea52
BigManBoss <teseopoma@gmail.com>
2022-09-29 16:29:54 +0200

    got the computer choice working

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+console.log("Hello World!");
+const GameOptions = ['Rock', 'Paper', 'Scissors'];
+function getComputerChoice() {
+    let i = Math.floor(Math.random() * 3);
+    return i;
+};
+
+console.log(GameOptions[getComputerChoice()]);
