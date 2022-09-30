bba7db3d396607e0ef2aff7d4b356708d2505d80
BigManBoss <teseopoma@gmail.com>
2022-09-30 16:10:51 +0200

    coded the gameplay

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-console.log("Hello World!");
 const GameOptions = ['Rock', 'Paper', 'Scissors'];
+
 function getComputerChoice() {
     let i = Math.floor(Math.random() * 3);
     return i;
 };
+function getPlayerChoice() {
 
-console.log(GameOptions[getComputerChoice()]);
+};
+
+var ComputerSelection = getComputerChoice();
+// var PlayerSelection = getPlayerChoice();
+
+var PlayerSelection = getPlayerChoice();
+
+function play(cpu, human) {
+    cpu = cpu.toLowerCase();
+    human = human.toLowerCase();
+    if (cpu === human) {
+        console.log("You both chose " + ComputerSelection + " , try again!");
+    } else if (human === "rock") {
+        if (cpu === "paper") {
+            console.log("The CPU chose Paper so you lost!!!");
+        } else {
+            console.log("The CPU chose Scissors so you won!!!");
+        };
+    } else if (human === "paper") {
+        if (cpu === "scissors") {
+            console.log("The CPU chose Scissors so you lost!!!");
+        } else {
+            console.log("The CPU chose Rock so you won!!!");
+        };
+    } else if (human === "scissors") {
+        if (cpu === "rock") {
+            console.log("The CPU chose Rock so you lost!!!");
+        } else {
+            console.log("The CPU chose Paper so you won!!!");
+        };
+    }
+};
