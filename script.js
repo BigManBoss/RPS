0b1fbc5240753f3c738a1c53614a4adbeb36656b
BigManBoss <teseopoma@gmail.com>
2022-10-01 18:03:37 +0200

    got working win board

--- script.js
+++ script.js
@@ -11,6 +11,25 @@ var ComputerSelection = "";
 // var PlayerSelection = getPlayerChoice();
 var PlayerSelection = "";
 
+//setting win varables
+var ComputerWins = 0;
+var PlayerWins = 0;
+
+function playerwins() {
+    let poop = "";
+    for (let i = PlayerWins; i > 0; i--) {
+        poop = poop + "X ";
+    };
+    return poop;
+};
+function computerwins() {
+    let poop = "";
+    for (let i = ComputerWins; i > 0; i--) {
+        poop = poop + "X ";
+    };
+    return poop;
+};
+
 //play function sees what the players chose and sees who wins
 //I loweredcased everything (I didnt need to)
 function play(cpu, human) {
@@ -21,20 +40,44 @@ function play(cpu, human) {
     } else if (human === "rock") {
         if (cpu === "paper") {
             document.getElementById("change").innerHTML = "The CPU chose Paper so you lost!!!";
+            ComputerWins++;
+            computerwins()
+            CheckWins()
+            return ComputerWins;
         } else {
             document.getElementById("change").innerHTML = "The CPU chose Scissors so you won!!!";
+            PlayerWins++;
+            playerwins();
+            CheckWins()
+            return PlayerWins; 
         };
     } else if (human === "paper") {
         if (cpu === "scissors") {
             document.getElementById("change").innerHTML = "The CPU chose Scissors so you lost!!!";
+            ComputerWins++;
+            computerwins()
+            CheckWins()
+            return ComputerWins;
         } else {
             document.getElementById("change").innerHTML = "The CPU chose Rock so you won!!!";
+            PlayerWins++;
+            playerwins();
+            CheckWins()
+            return PlayerWins;
         };
     } else if (human === "scissors") {
         if (cpu === "rock") {
             document.getElementById("change").innerHTML = "The CPU chose Rock so you lost!!!";
+            ComputerWins++;
+            computerwins()
+            CheckWins()
+            return ComputerWins;
         } else {
             document.getElementById("change").innerHTML = "The CPU chose Paper so you won!!!";
+            PlayerWins++;
+            playerwins();
+            CheckWins()
+            return PlayerWins;
         };
     };
     
@@ -45,14 +88,21 @@ function rock() {
     PlayerSelection = "Rock"
     ComputerSelection = getComputerChoice();
     play(ComputerSelection, PlayerSelection);
-}
+};
 function paper() {
     PlayerSelection = "Paper"
     ComputerSelection = getComputerChoice();
     play(ComputerSelection, PlayerSelection);
-}
+};
 function scissors() {
     PlayerSelection = "Scissors"
     ComputerSelection = getComputerChoice();
     play(ComputerSelection, PlayerSelection);
-}
+};
+
+
+
+function CheckWins() {
+    document.getElementById("p").innerHTML = "Player: " + playerwins();
+    document.getElementById("c").innerHTML = "CPU: " + computerwins();
+};
